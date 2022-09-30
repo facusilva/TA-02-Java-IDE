03fee02e2d9ce7af3ba219a1a9cc77683dfa2438
facusilva <facun772@gmail.com>
2022-09-30 12:23:52 +0200

    Commit de prueba

--- TA02-ComentarioLinea/src/ComentarioLinea.java
+++ TA02-ComentarioLinea/src/ComentarioLinea.java
@@ -2,7 +2,7 @@
 public class ComentarioLinea {
 
 	public static void main(String[] args) {
-		// System.out.println("Hola");	
+		// System.out.println("Hola") ;	
 		System.out.println("Adios");	
 		System.out.println("FIN DE PROGRAMA");	
 	}
